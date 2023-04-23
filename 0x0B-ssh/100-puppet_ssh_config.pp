d30fdc3f91c05b5d8ce82a5e1712bc67a1fc9c92
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-23 17:48:57 +0200

    Changing the dir

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # puppet manifest to create/modify content of ssh client config
 file {'ssh_school':
 ensure  => present,
-path    => '/home/ussumane/.ssh/config',
+path    => '/etc/ssh/ssh_config',
 content => 'Host 34.202.159.196
 IdentityFile ~/.ssh/school
 PasswordAuthentication no'
