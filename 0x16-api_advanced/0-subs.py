b107bb33be70336157b660ce665c909f90c0b47a
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-08-08 14:21:38 +0200

    fix: not using the Authorization to get the reddit subrredit information

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,11 +2,8 @@
 """
 Querying reddit API
 """
-from credentials import USERNAME, PASSWORD
+# from credentials import USERNAME, PASSWORD, CLIENT_ID, SECRET_KEY
 import requests
-
-CLIENT_ID = "SVe5BCuAok6qOTVn23R2SA"
-SECRET_KEY = "N19GAR7xI5RdVZCofw7WNR61uLIUyQ"
 
 
 def number_of_subscribers(subreddit):
@@ -16,21 +13,22 @@ def number_of_subscribers(subreddit):
         - number of subscribers
     """
     # Authenticate
-    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
-    data = {
-            'grant_type': 'password',
-            'username': USERNAME,
-            'password': PASSWORD
-            }
+    # auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
+    # data = {
+    #        'grant_type': 'password',
+    #        'username': USERNAME,
+    #        'password': PASSWORD
+    #        }
     headers = {'User-Agent': 'A red automation script 1.1 by Uss'}
 
     # get token access id
-    ACCESS_URL = "https://www.reddit.com/api/v1/access_token"
-    res = requests.post(ACCESS_URL, data=data, headers=headers, auth=auth)
-    TOKEN = res.json()['access_token']
-    headers = {**headers, **{'Authorization': f'bearer {TOKEN}'}}
+    # ACCESS_URL = "https://www.reddit.com/api/v1/access_token"
+    # res = requests.post(ACCESS_URL, data=data, headers=headers, auth=auth)
+    # TOKEN = res.json()['access_token']
+    # headers = {**headers, **{'Authorization': f'bearer {TOKEN}'}}
 
-    URL_API = "https://oauth.reddit.com/r/{}/about".format(subreddit)
+    # URL_API = "https://oauth.reddit.com/r/{}/about".format(subreddit)
+    URL_API = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     resp = requests.get(URL_API, headers=headers)
     if resp.status_code == 200:
         subs_count = resp.json()['data']['subscribers']
